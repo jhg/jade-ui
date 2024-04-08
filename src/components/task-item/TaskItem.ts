6d26e81c674e3bf9046658c7c0251cc05985cb05
Jesus Hernandez <jesushdez@protonmail.com>
2024-04-08 07:59:42 +0200

    CSS refactor

--- src/components/task-item/TaskItem.ts
+++ src/components/task-item/TaskItem.ts
@@ -22,46 +22,53 @@ export class TaskItem extends LitElement {
                 border-color: var(--border-color-primary);
                 border-width: 1px;
                 border-style: solid;
-                border-radius: var(--border-radius);
+                border-radius: var(--component-radius);
                 padding: 0.5em;
                 overflow: hidden;
                 height: 1.25em;
             }
+
             :host * {
                 margin: 0;
                 padding: 0;
             }
+
             :host label {
                 width: 100%;
             }
+
             :host input[type=checkbox] {
                 width: 1em;
                 height: 1em;
                 cursor: pointer;
-                background-color: rgba(var(--color-primary), 0.2);
+                background-color: rgba(var(--component-primary), 0.1);
                 border-color: var(--border-color-primary);
                 border-width: 1px;
                 border-style: solid;
-                border-radius: var(--border-radius);
+                border-radius: var(--component-radius);
                 appearance: none;
                 vertical-align: -15%;
                 font-size: 1em;
-                color: rgb(var(--color-primary));
+                color: rgb(var(--component-primary));
                 top: 0.15em;
                 position: relative;
             }
+
             :host input[type=checkbox]::before {
                 top: -0.3em;
                 position: relative;
                 content: '✔';
                 color: transparent;
             }
+
             :host input[type=checkbox]:checked {
                 background-color: transparent;
             }
+
             :host input[type=checkbox]:checked::before {
-                color: rgb(var(--color-primary));
+                color: rgb(var(--component-primary));
             }
+
             :host input[type=text] {
                 width: calc(100% - 2em);
                 border: none;
